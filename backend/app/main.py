a28faf8f1f504c42c396b8328764883a09d6edd6
Kunle123 <kunle2000@gmail.com>
2025-04-05 19:34:43 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://perfect-cv-snowy.vercel.app"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 ) 
